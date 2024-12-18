044fe3b44aabf5959eae556d45fa0b41515df212
martin notebook <martinjoameza@gmail.com>
2024-12-18 12:47:38 -0300

    ihstorial retoques

--- src/components/HistorialVentas.jsx
+++ src/components/HistorialVentas.jsx
@@ -10,6 +10,17 @@ function HistorialVentas({ setMenu, setHistory }) {
     setHistory([]);
   };
 
+  const handleDeleteVenta = (venta) => {
+    // Filtrar las ventas que no coinciden con la venta que queremos eliminar
+    const newHistory = history.filter((ventaNoActual) => {
+      return ventaNoActual[0].FechaYHora !== venta[0].FechaYHora;
+    });
+
+    // Actualizar el estado y el localStorage con el historial filtrado
+    localStorage.setItem("historial", JSON.stringify(newHistory));
+    setHistory(newHistory);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center w-[100vw] h-[100vh] text-white px-4">
       <div className="w-[90vw] h-[90vh] bg-black bg-opacity-80 flex items-center justify-center flex-col">
@@ -40,19 +51,27 @@ function HistorialVentas({ setMenu, setHistory }) {
                 <tbody>
                   {history.map((venta, index) => (
                     <>
-                      <tr>
+                      <tr className="relative">
                         <td
-                          colSpan={5}
-                          className="border-2 px-4 py-2 border-black bg-green-200"
+                          colSpan={4}
+                          className="border-2 cursor-pointer px-4 py-2 border-black bg-green-200"
                         >
                           Venta {index + 1}
+                        </td>
+                        <td
+                          onClick={() => handleDeleteVenta(venta)}
+                          className="cursor-pointer bg-red-500 hover:bg-red-600 border-2 border-black"
+                        >
+                          X
                         </td>
                       </tr>
                       {venta.map((item) => {
                         return (
                           <tr key={item.FechaYHora}>
                             <td className="border border-black py-2">
-                              {item.FechaYHora ? new Date(item.FechaYHora).toLocaleString(): ''}
+                              {item.FechaYHora
+                                ? new Date(item.FechaYHora).toLocaleString()
+                                : ""}
                             </td>
                             <td className="border border-black py-2">
                               {item.name}
@@ -70,10 +89,16 @@ function HistorialVentas({ setMenu, setHistory }) {
                         );
                       })}
                       <tr>
-                        <td className="bg-blue-200 border border-slate-500  ">{ new Date(venta[0].FechaYHora).getHours() >= 9 && new Date(venta[0].FechaYHora).getHours() < 13 ? 
-                        'Turno Mañana':'Turno Tarde'}</td>
-                        <td className="bg-blue-200  border border-slate-500  ">{venta[0].Tipo}</td>
-                        <td className="bg-blue-200  border border-slate-500  "></td>
+                        <td className="bg-blue-200 border border-slate-500">
+                          {new Date(venta[0].FechaYHora).getHours() >= 9 &&
+                          new Date(venta[0].FechaYHora).getHours() < 13
+                            ? "Turno Mañana"
+                            : "Turno Tarde"}
+                        </td>
+                        <td className="bg-blue-200 border border-slate-500">
+                          {venta[0].Tipo}
+                        </td>
+                        <td className="bg-blue-200 border border-slate-500"></td>
                         <td className="border-2 border-black px-4 py-2">
                           Total Venta
                         </td>
@@ -89,60 +114,66 @@ function HistorialVentas({ setMenu, setHistory }) {
                   ))}
                 </tbody>
               </table>
-              
             </div>
           ) : (
             <p className="text-xl text-center p-5">
               No hay ventas registradas en el historial.
             </p>
           )}
-          
         </div>
         <div className="grid grid-cols-3 text-2xl py-2 w-1/2">
-                <h3 className =' text-center'>Turno Mañana</h3>
-                <h3 className =' text-center'>Turno Tarde</h3>
-                <h3 className =' text-center'>Total</h3>
+          <h3 className="text-center">Turno Mañana</h3>
+          <h3 className="text-center">Turno Tarde</h3>
+          <h3 className="text-center">Total</h3>
 
-                {/* Total de ventas para Turno Mañana */}
-                <h3 className =' text-center  '>$
-                {history.filter((venta)=>{
-                    let hora = new Date(venta[0].FechaYHora).getHours();
-                    return hora > 9 && hora < 13;
-                  }).reduce((acc, curr) => {
-                    let sum =
-                      acc + curr.reduce((acc, curr) => {
-                          return acc + curr.precioTotal; // Sumar el precioTotal de cada item
-                        }, 0);
-                    return sum;
-                  }, 0)}
-                </h3>
+          {/* Total de ventas para Turno Mañana */}
+          <h3 className="text-center">
+            $
+            {history
+              .filter((venta) => {
+                let hora = new Date(venta[0].FechaYHora).getHours();
+                return hora > 9 && hora < 13;
+              })
+              .reduce((acc, curr) => {
+                let sum =
+                  acc +
+                  curr.reduce((acc, curr) => {
+                    return acc + curr.precioTotal; // Sumar el precioTotal de cada item
+                  }, 0);
+                return sum;
+              }, 0)}
+          </h3>
 
-                {/* Total de ventas para Turno Tarde */}
-                <h3 className =' text-center  '>$
-                {history.filter((venta)=>{
-                    let hora = new Date(venta[0].FechaYHora).getHours();
-                    return hora > 17 && hora < 21;
-                  }).reduce((acc, curr) => {
-                    let sum =
-                      acc + curr.reduce((acc, curr) => {
-                          return acc + curr.precioTotal; // Sumar el precioTotal de cada item
-                        }, 0);
-                    return sum;
-                  }, 0)}
-                </h3>
+          {/* Total de ventas para Turno Tarde */}
+          <h3 className="text-center">
+            $
+            {history
+              .filter((venta) => {
+                let hora = new Date(venta[0].FechaYHora).getHours();
+                return hora > 17 && hora < 21;
+              })
+              .reduce((acc, curr) => {
+                let sum =
+                  acc +
+                  curr.reduce((acc, curr) => {
+                    return acc + curr.precioTotal; // Sumar el precioTotal de cada item
+                  }, 0);
+                return sum;
+              }, 0)}
+          </h3>
 
-                <h3 className =' text-center'>$
-                {history.reduce((acc, curr) => {
-                    let sum =
-                      acc +
-                      curr
-                        .reduce((acc, curr) => {
-                          return acc + curr.precioTotal; // Sumar el precioTotal de cada item
-                        }, 0);
-                    return sum;
-                  }, 0)}
-                </h3>
-              </div>
+          <h3 className="text-center">
+            $
+            {history.reduce((acc, curr) => {
+              let sum =
+                acc +
+                curr.reduce((acc, curr) => {
+                  return acc + curr.precioTotal; // Sumar el precioTotal de cada item
+                }, 0);
+              return sum;
+            }, 0)}
+          </h3>
+        </div>
         <div className="flex flex-col sm:flex-row justify-between mt-5 w-full sm:w-[90%] space-y-3 sm:space-y-0 sm:space-x-3">
           <button
             onClick={() => setMenu("")}
